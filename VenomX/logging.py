1bbc51c5d90f308b9a0a0b5fdc57ea78f9890240
amiralizai2006-stack <amiralizai2006@gmail.com>
2026-09-21 22:06:36 +0430

    Update logging.py

--- VenomX/logging.py
+++ VenomX/logging.py
@@ -1,22 +1,18 @@
-
 # All rights reserved.
 
 
 import logging
 from logging.handlers import RotatingFileHandler
 
-from config import LOG_FILE_NAME
-
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
-        RotatingFileHandler(LOG_FILE_NAME, maxBytes=5000000, backupCount=10),
+        RotatingFileHandler("/tmp/VenomXlogs.txt", maxBytes=5000000, backupCount=10),
         logging.StreamHandler(),
     ],
 )
-
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 logging.getLogger("pymongo").setLevel(logging.ERROR)
